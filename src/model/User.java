49ce1d3e4108a7aaac1d6bed16a8f29727a85aa4
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-11-18 01:38:35 -0500

    Added string arg constructor to User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,15 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User implements Serializable{
 	private String name;
 	List<Album> myAlbums;
+	
+	public User(String name) {
+		this.name = name;
+		myAlbums = new ArrayList<Album>();
+	}
 }
