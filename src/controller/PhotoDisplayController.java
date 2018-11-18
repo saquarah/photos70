57ac5a7230f5d584b01cfc41ea19d2ed25caf4c0
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-11-18 16:18:38 -0500

    Added fx id's and empty methods

--- src/controller/PhotoDisplayController.java
+++ src/controller/PhotoDisplayController.java
@@ -1,5 +1,48 @@
 package controller;
 
-public class PhotoDisplayController extends Controller{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
+public class PhotoDisplayController extends Controller{
+	
+	@FXML
+	Button prevPhotoB, nextPhotoB, backToAlbumB, logoutB, quitB;
+	
+	@FXML
+	Label albumLabel, dateLabel;
+	
+	@FXML
+	TextArea tagTxtArea;
+	
+	@FXML
+	ImageView imageView;
+	
+	@FXML
+	public void prevPhoto(ActionEvent e) {
+		
+	}
+	
+	@FXML
+	public void nextPhoto(ActionEvent e) {
+		
+	}
+	
+	@FXML
+	public void backToAlbum(ActionEvent e) {
+		
+	}
+	
+	@FXML
+	public void logout(ActionEvent e) {
+		
+	}
+	
+	@FXML
+	public void quit(ActionEvent e) {
+		
+	}
 }
