a9266a6ec5a1c10ff4cca0f4ee3de5054eae51a9
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-11-19 01:50:07 -0500

    Added thumbnail, image, date to Photo

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -1,11 +1,27 @@
 package model;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.Calendar;
+
+import javafx.scene.image.Image;
 public class Photo implements Serializable{
 	
-	private String photoTitle;
+	private String caption;
 	private Album rootAlbum;
+	private Image image;
+	private Image thumbnail;
+	private File file;
 	Calendar date = Calendar.getInstance();
 	
+	public Photo(Image image, Calendar date, File file) {
+		this.image = image;
+		this.date = date;
+		this.file = file;
+		this.thumbnail = createThumbnail();
+	}
+	private Image createThumbnail() {
+		Image thumbnail = new Image(file.getAbsolutePath(), 100, 100, false, false);
+		return thumbnail;
+	}
 }
