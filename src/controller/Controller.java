a3ccf32a6cce1b644059a3374abe3bd41db0ba1e
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-11-20 15:57:59 -0500

    fixed serialization issue where there would be an error if there was no UserList.dat file

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -55,7 +55,7 @@ public class Controller {
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+	//		e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
