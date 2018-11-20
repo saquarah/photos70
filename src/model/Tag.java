bbd972d63d024a57ab30bf669b7604c70f9958a5
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-11-19 23:08:29 -0500

    Implemented Tag.java

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Tag {
+import java.io.Serializable;
 
+public class Tag implements Serializable{
+	private String type, value;
+	private Photo photo;
+	public Tag(String type, String value, Photo photo) {
+		super();
+		this.type = type;
+		this.value = value;
+		this.photo = photo;
+	}
+	public String getTag() {
+		return type;
+	}
+	public String getValue() {
+		return value;
+	}
+	public Photo getPhoto() {
+		return photo;
+	}
+	
+	
+	
 }
